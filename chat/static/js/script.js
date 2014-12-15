05238644cb2d0f3e49c69927b9ef228e91a49c5c
lionn2 <_sanyok_@ukr.net>
2014-12-16 00:40:46 +0200

    ___

--- chat/static/js/script.js
+++ chat/static/js/script.js
@@ -1,16 +1,14 @@
-alert('hi')
 var user = {}
+var chats = []
 user.name = prompt('Enter your name');
 // Пример с POST
 function createUser() {
 	var xhr = new XMLHttpRequest();
 
-	var token = document.getElementsByName('csrfmiddlewaretoken')[0].value;
-	//alert(token);
+	var token = document.getElementsByName('csrfmiddlewaretoken')[0].value;\
 	var params = 'csrfmiddlewaretoken=' + token + '&name=' + user.name;
-	
+
 	xhr.open("POST", '/create_user/', true);
-	//xnr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
 	xhr.onreadystatechange = function() {
@@ -18,19 +16,7 @@ function createUser() {
 	  		alert(this.readyState)
 	  		return;
 		}
-	  alert(this.responseText);
 	}
 
 	xhr.send(params);
-}
-function createUser3 () {
-	var params = 'name=' + encodeURIComponent(user.name);
-	$.ajax({
-      type: "post",
-      url: "/create_user/",
-      data: params,
-      success: function(data) {
-        alert(data);
-      }
-    });	
 }
