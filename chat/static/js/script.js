8270ccf23979da7381bd80fa8c60a97a43c94574
lionn2 <_sanyok_@ukr.net>
2014-12-15 22:50:38 +0200

    create post ajax

--- chat/static/js/script.js
+++ chat/static/js/script.js
@@ -2,13 +2,32 @@ alert('hi')
 var user = {}
 user.name = prompt('Enter your name');
 // Пример с POST
-var xhr = new XMLHttpRequest();
+function createUser2() {
+	var xhr = new XMLHttpRequest();
 
-var params = 'name=' + encodeURIComponent(name);
+	var params = 'name=' + encodeURIComponent(user.name);
 
-xhr.open("POST", '/create_user', true)
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+	xhr.open("POST", '/create_user/', true)
+	//xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
 
-xhr.onreadystatechange = ...;
+	xhr.onreadystatechange = function() {
+	  	if (this.readyState != 4) {
+	  		alert(this.readyState)
+	  		return;
+		}
+	  alert(this.responseText);
+	}
 
-xhr.send(params);
+	xhr.send(params);
+}
+function createUser () {
+	var params = 'name=' + encodeURIComponent(user.name);
+	$.ajax({
+      type: "post",
+      url: "/create_user/",
+      data: params,
+      success: function(data) {
+        alert(data);
+      }
+    });	
+}
