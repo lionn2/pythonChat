f8135958ffc2a8b243545c93b8ab12c5a90cc9e9
lionn2 <_sanyok_@ukr.net>
2014-12-15 22:28:56 +0200

    add post create_user

--- chat/static/js/script.js
+++ chat/static/js/script.js
@@ -1,3 +1,14 @@
 alert('hi')
 var user = {}
 user.name = prompt('Enter your name');
+// Пример с POST
+var xhr = new XMLHttpRequest();
+
+var params = 'name=' + encodeURIComponent(name);
+
+xhr.open("POST", '/create_user', true)
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+
+xhr.onreadystatechange = ...;
+
+xhr.send(params);
