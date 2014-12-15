89733916760023aee076bc8ee12bc3215a7dcaee
andriy <andriyletsyk1993>
2014-12-15 22:48:54 +0200

    method create user with url and simple post and get

--- OurChat/urls.py
+++ OurChat/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+import chat
+from chat import views
 
 
 urlpatterns = patterns('',
@@ -12,4 +14,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'chat.views.index', name='index'),
 	url(r'^create_user/$', 'chat.views.create_user', name='create_user'),
+	url(r'^name/$', 'chat.views.name', name='name'),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
