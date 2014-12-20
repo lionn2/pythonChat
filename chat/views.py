9f0213e5281ecabd1c2bb418f786ddcd0603504f
andriy <andriyletsyk1993>
2014-12-20 17:02:34 +0200

    authorization2

--- chat/views.py
+++ chat/views.py
@@ -35,8 +35,8 @@ def create_user(request):
 	user.save()
 	user = authenticate(username=username, password=password)
 	if user is not None:
-        if user.is_active:
-            login(request, user)
+		if user.is_active:
+    		#login(request, user)
 			user_to_json = json.dumps(user.to_json())
 			return HttpResponse(user_to_json)
 		else:
