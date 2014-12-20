526a7fd82ef4004ed4736a5355721381062975b9
lionn2 <_sanyok_@ukr.net>
2014-12-20 18:09:21 +0200

    0_0

---
+++ chat/static/js/registration.js
@@ -0,0 +1,15 @@
+function f () {
+	console.log('js');
+	$.ajax({
+           type: "POST",
+           url: '../create_user/',
+           data: $('#reg_form').serialize(), // serializes the form's elements.
+           success: function(messages)
+           {
+           		messages = jQuery.parseJSON(messages);
+           		//addMessagesToTable(messages);
+                textArea.value = "";
+           }
+         });
+}
+console.log('reg')
